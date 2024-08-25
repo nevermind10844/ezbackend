4ded827ddb4819b55a171670cef0e08c451b9a97
nevermind10844 <jan.kressin@googlemail.com>
2024-08-25 16:39:51 +0200

    property type enumeration in property details

--- src/main/java/com/jksoft/ezbackend/controller/PropertyController.java
+++ src/main/java/com/jksoft/ezbackend/controller/PropertyController.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.fasterxml.jackson.databind.ser.impl.PropertySerializerMap;
 import com.jksoft.ezbackend.config.security.user.CustomUserDetails;
 import com.jksoft.ezbackend.config.security.user.User;
 import com.jksoft.ezbackend.config.security.user.UserService;
 import com.jksoft.ezbackend.entities.Company;
-import com.jksoft.ezbackend.entities.Item;
 import com.jksoft.ezbackend.entities.Property;
-import com.jksoft.ezbackend.entities.PropertyType;
+import com.jksoft.ezbackend.error.StructureObjectNotFoundException;
+import com.jksoft.ezbackend.error.BadRequestException;
 import com.jksoft.ezbackend.service.ItemService;
 import com.jksoft.ezbackend.service.PropertyService;
 
@@ -23,10 +22,10 @@ import com.jksoft.ezbackend.service.PropertyService;
 public class PropertyController {
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	ItemService itemService;
-	
+
 	@Autowired
 	PropertyService propertyService;
 
@@ -36,11 +35,34 @@ public class PropertyController {
 		CustomUserDetails cud = (CustomUserDetails) principal;
 		User user = userService.read(cud.getId());
 		Company company = user.getCompany();
-		
+
 		Property property = propertyService.readProperty(company, propertyId);
 		model.addAttribute("property", property);
-		
+
 		return "structure/property/admin/propertyDetails";
+
+	}
+
+	@PostMapping("/admin/property/{propertyId}")
+	public String updateProperty(Model model, @PathVariable Long propertyId, Property property) {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		CustomUserDetails cud = (CustomUserDetails) principal;
+		User user = userService.read(cud.getId());
+		Company company = user.getCompany();
+
+		if (propertyId.equals(property.getId())) {
+			Property remoteProperty = propertyService.readProperty(company, propertyId);
+			if (remoteProperty == null)
+				throw new StructureObjectNotFoundException();
+			
+			property.setCompany(remoteProperty.getCompany());
+			property.setNamespace(remoteProperty.getNamespace());
+			property.setItem(remoteProperty.getItem());
+			propertyService.updateProperty(property);
+		} else {
+			throw new BadRequestException();
+		}
 		
+		return String.format("redirect:/admin/property/%d", propertyId);
 	}
 }
